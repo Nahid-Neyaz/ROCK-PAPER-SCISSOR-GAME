328c0e98725433cc877c6d05c232822a97012379
Nahid Neyaz <nahidneyaz2906@gmail.com>
2025-01-30 13:28:00 +0530

    abc

--- p3.js
+++ p3.js
@@ -1,10 +1,17 @@
 const buttons= document.querySelectorAll('button');
 
+const resultE1 = document.getElementById("result");
+
+let playerscore = 0;
+let computerscore = 0;
+
+const psE1=document.getElementById("user-score");
+const csE1=document.getElementById("c-score");
+
 buttons.forEach(button => {
     button.addEventListener('click',()=>{
-        console.log('user choice', button.id,
-            "computer choice",cplay()
-        );
+      const r= playRound(button.id,cplay());
+      resultE1.innerHTML=r;
 });
 });
 
@@ -15,6 +22,19 @@ function  cplay() {
     return choices[randomChoice]   
 }
 
-function playRound(player){
-    
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection){
+        return("It's a tie");}
+    else if((playerSelection == "rock" && computerSelection == "Scissors")|| (playerSelection == "Scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock") )
+    {
+        playerscore++;
+        psE1.innerHTML=playerscore;
+        return ("You win! " + playerSelection + " beats " + computerSelection);
+    }
+    else {
+        computerscore++;
+        csE1.innerHTML=computerscore;
+        return ("You lose! " + computerSelection + " beats " + playerSelection);
+    }
+
 }
